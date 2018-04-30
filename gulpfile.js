5987e15e123ce4e789babd4af2382997748d1af4
Aung Myo Kyaw <me@aungmyokyaw.com>
2018-04-30 20:54:27 +0630

    mcal: transpile to es5

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('node', () => {
+  return gulp
+    .src('src/**')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/node'));
+});
+
+gulp.task('default', ['node']);
